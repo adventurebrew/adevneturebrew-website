eff140276b36441133a7dbe907edb3598b553920
haggai <yhaggai@gmail.com>
2020-04-17 22:22:24 +0300

    adding props to base components

--- src/components/base-components/box.js
+++ src/components/base-components/box.js
@@ -1,7 +1,39 @@
-import styled from '@emotion/styled'
-import PropTypes from 'prop-types'
-import { border, space, color, layout, fontSize, typography } from 'styled-system'
-import systemPropTypes from '@styled-system/prop-types'
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import {
+  border,
+  space,
+  color,
+  layout,
+  fontSize,
+  typography,
+  position,
+  shadow,
+  system,
+} from 'styled-system';
+import systemPropTypes from '@styled-system/prop-types';
+
+const gridProps = system({
+  alignSelf: {
+    property: 'align-self',
+  },
+  justifySelf: {
+    property: 'justify-self',
+  },
+  gridArea: {
+    property: 'grid-area',
+  },
+  gridColumn: {
+    property: 'grid-column',
+  },
+  gridRow: {
+    property: 'grid-row',
+  },
+  zIndex: {
+    property: 'z-index',
+    scale: 'zIndex',
+  },
+});
 
 const Box = styled.div`
   ${layout}
@@ -10,12 +42,15 @@ const Box = styled.div`
   ${fontSize}
   ${typography}
   ${border}
-`
+  ${position}
+  ${shadow}
+  ${gridProps}
+`;
 
 Box.propTypes = {
   ...systemPropTypes.layout,
   ...systemPropTypes.space,
   ...systemPropTypes.color,
-}
+};
 
-export default Box
+export default Box;
